7dd6b7fb193f7820b98902eb199efd7a2fa789f7
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-12 11:47:35 +0200

    Add admin filter path

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
