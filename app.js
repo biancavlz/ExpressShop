f85afe72cdae532203a9a7da01b23e8306197ac4
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-12 11:23:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>HOME</h1>");
+});
+
+app.listen(3001);
