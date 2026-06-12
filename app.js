6144c6bf88ead1f087f8b72c43044d7860209d0e
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-12 13:49:42 +0200

    Styling - Add static middleware to handle external css files

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require("node:path");
 
 const express = require("express");
 
-const rootDir = require("./utils/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -10,12 +9,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3001);
