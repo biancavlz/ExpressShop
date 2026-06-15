026c301bd12959b08a524a86ef5f7dd492d26bd2
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-15 17:01:29 +0200

    add ejs as template engine and applies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require("node:path");
 
 const express = require("express");
-const { engine } = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,16 +9,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-  }),
-);
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,7 +19,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
+  res.status(404).render("404", { pageTitle: "Page not found", path: req.url });
 });
 
 app.listen(3001);
