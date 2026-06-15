fc6c046f0f2f789bc0882fad258221b6776b8439
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-15 11:15:10 +0200

    Install PUG as template engine, get dynamic Products data into shop

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
