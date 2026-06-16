68fe1ea30cf81645986d22f69da85e828d27bfd7
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-16 10:49:32 +0200

    Controllers, separate logic into controllers

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require("node:path");
 
 const express = require("express");
+const errorController = require("./controllers/error");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
@@ -15,11 +16,9 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found", path: req.url });
-});
+app.use(errorController.get404);
 
 app.listen(3001);
--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,11 @@
 const path = require("node:path");
 
 const express = require("express");
-const rootDir = require("../utils/path");
+
+const productsController = require("../controllers/products");
 const router = express.Router();
-const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
+router.post("/add-product", productsController.postAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
