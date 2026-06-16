64d379e464f068169fc6372296c71801840cf0ca
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-16 13:38:03 +0200

    Rename controllers and add more views and routes

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,11 @@
+const path = require("../utils/path");
+
 exports.get404 = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found", path: req.url });
+  res
+    .status(404)
+    .render("404", {
+      pageTitle: "Page not found",
+      path: req.url,
+      path: "/404",
+    });
 };
