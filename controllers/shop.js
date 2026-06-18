1629abbe69bbd0fe3116f049210240f29336c2e4
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-18 11:09:49 +0200

    route, controller - product add to cart functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,6 +41,12 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  res.redirect("/cart");
+};
+
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
