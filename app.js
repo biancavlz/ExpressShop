281c8ad328c1594a68d3bb724874c845a022a81e
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-23 11:33:57 +0200

    Sequelize - Add user/product association, force sync only for dev env

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const errorController = require("./controllers/error");
 const sequelize = require("./utils/database");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -22,7 +25,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Associations
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(app.listen(3001))
   .catch((err) => console.log(err));
