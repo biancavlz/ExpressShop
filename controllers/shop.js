bde383e8f756c695a1d85cdcf2f1727aa1b7a1db
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-24 10:32:52 +0200

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,12 +65,11 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  // const prodId = req.body.productId;
+  // Product.findById(prodId, (product) => {
+  //   Cart.addProduct(prodId, product.price);
+  // });
+  // res.redirect("/cart");
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
