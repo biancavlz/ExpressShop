ac8f0a50f0d33b5b68aa3dfbd709d3820024adf7
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-25 10:25:17 +0200

    MongoDB - connection configuration

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
+require("dotenv").config();
 const path = require("node:path");
 
 const express = require("express");
 const errorController = require("./controllers/error");
-const sequelize = require("./utils/database");
-
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoDBConnect = require("./utils/database").mongoDBConnect;
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
 const { create } = require("node:domain");
@@ -26,47 +20,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
+  next();
 });
 
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Associations
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true }) //To restart the database with no data
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "John Doe", email: "john.doe@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3001);
-  })
-  .catch((err) => console.log(err));
+mongoDBConnect(() => {
+  app.listen(3001);
+});
