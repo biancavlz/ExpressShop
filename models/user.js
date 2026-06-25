26c223173946c5b4d5c78c9004cbc5f8281801a7
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-25 16:56:43 +0200

    MongoDB - Get and display cart items

--- models/user.js
+++ models/user.js
@@ -45,6 +45,27 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDB();
+    const productIds = this.cart.items.map((item) => {
+      return item.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
   static findById(userId) {
     const db = getDB();
 
