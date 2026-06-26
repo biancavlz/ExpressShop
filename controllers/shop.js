6c5a4c84f03b207070c4cc5ae1e87819cd111dbd
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-26 14:39:18 +0200

    Mongoose - get cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -47,9 +47,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
-      console.log(products);
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
