a486ee57a3c13b9d0e39565e41ffa464fe578d51
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-26 11:43:20 +0200

    Mongoose - Delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -79,7 +79,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
 
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       "Product was deleted";
       res.redirect("/admin/products");
