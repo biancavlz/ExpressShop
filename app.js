2dec9f2bf66fcc981e10c5afce7ce169a9a7eb24
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-26 12:04:16 +0200

    Mongoose - create temporal fake user

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoDBConnect = require("./utils/database").mongoDBConnect;
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const bodyParser = require("body-parser");
 const { create } = require("node:domain");
@@ -21,14 +21,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("6a3d23d586dcb1fe90d47807")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("6a3e4de333135a8302288e37")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -36,5 +36,18 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoDBConnect(() => {
+  User.findOne().then((user) => {
+    if (!user) {
+      const user = new User({
+        name: "John Doe",
+        email: "john.doe@test.com",
+        cart: {
+          items: [],
+        },
+      });
+      user.save();
+    }
+  });
+
   app.listen(3001);
 });
