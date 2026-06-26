c8c666f43659ac767d8e54a32d4d7ad24a003c63
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-26 11:51:20 +0200

    Moongoose - Create User model schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,28 @@
+const mongoose = require("mongoose");
+const product = require("./product");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
+
 // const mongodb = require("mongodb");
 
 // const getDB = require("../utils/database").getDB;
