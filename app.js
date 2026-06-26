6c668bf12bc7b300ba3a77631eda9dd01a2efce8
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-26 11:22:25 +0200

    Disable fake user

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoDBConnect = require("./utils/database").mongoDBConnect;
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const User = require("./models/user");
+// const User = require("./models/user");
 
 const bodyParser = require("body-parser");
 const { create } = require("node:domain");
@@ -21,14 +21,14 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("6a3d23d586dcb1fe90d47807")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("6a3d23d586dcb1fe90d47807")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
