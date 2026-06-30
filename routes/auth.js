ef63e93aded2db4619a12e582f6e5e2988cce886
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-30 11:23:05 +0200

    Authentocation flow - Add signup form and MVC

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.post("/signup", authController.postSignup);
+router.get("/signup", authController.getSignup);
 router.post("/logout", authController.postLogout);
 
 module.exports = router;
