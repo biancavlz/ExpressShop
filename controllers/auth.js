d7231bf7ce73c0fd9f4b43e575dd56d143379d2f
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-07-01 15:44:19 +0200

    Validations - Add validation to signup form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
@@ -63,7 +64,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
 
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const { check, body } = require("express-validator");
+
 const authController = require("../controllers/auth");
 
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email"),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and with at least 4 characters",
+    )
+      .isLength({ min: 4 })
+      .isAlphanumeric(),
+    // body("confirmPassword").custom((value, { req }) => {
+    //   if (value !== req.body.password) {
+    //     throw new Error("Passwords have to match");
+    //   }
+    //   return true;
+    // }),
+  ],
+  authController.postSignup,
+);
 router.get("/signup", authController.getSignup);
 router.post("/logout", authController.postLogout);
 
