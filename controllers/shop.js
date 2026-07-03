40a2214f92000b74773e2f87a25650e1fac45b97
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-07-03 11:55:16 +0200

    Invoices - Generates PDF dummy with pdfkit library

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
+const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -169,24 +170,19 @@ exports.getInvoice = (req, res, next) => {
       }
 
       const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
 
-      const invoicePath = path.join("data", "invoices", "invoiceName");
-      // fs.readFile(invoicePath, (err, data) => {
-      //   if (err) {
-      //     return next(err);
-      //   }
-      //   res.setHeader("Content-Type", "application/pdf");
-      //   res.setHeader(
-      //     'Content-Disposition", "inline; filename="' + invoiceName + '"',
-      //   );
-      //   res.send(data);
-      // });
-      const file = fs.createReadStream(invoicePath);
+      const pdfDoc = new PDFDocument();
       res.setHeader("Content-Type", "application/pdf");
       res.setHeader(
-        'Content-Disposition", "inline; filename="' + invoiceName + '"',
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"',
       );
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text("hello world");
+      pdfDoc.end();
     })
     .catch((err) => next(err));
 };
