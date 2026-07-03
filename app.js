885146f9771fa7b233178218670e269c4f879f1e
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-07-03 09:48:02 +0200

    Images - Accept or rejects specific image formats

--- app.js
+++ app.js
@@ -25,6 +25,17 @@ const fileStorage = multer.diskStorage({
     cb(null, new Date().toISOString() + "-" + file.originalname);
   },
 });
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 const bodyParser = require("body-parser");
 const { create } = require("node:domain");
 
@@ -34,7 +45,9 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image"));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"),
+);
 
 app.use(express.static(path.join(__dirname, "public")));
 
