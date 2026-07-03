2ed411bcae176ef183b75ffdd4fdf793ad4a97d1
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-07-03 11:04:24 +0200

    Invoices - Create route and controller to download invoice

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -151,4 +154,17 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = "invoice-" + orderId + ".pdf";
+
+  const invoicePath = path.join("data", "invoices", "invoiceName");
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.send(data);
+  });
 };
