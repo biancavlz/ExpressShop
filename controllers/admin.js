a58e108bd516f52f4c180d785a4100fea408a18f
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-07-05 12:53:41 +0200

    Delete product without reloading the page

--- controllers/admin.js
+++ controllers/admin.js
@@ -158,8 +158,9 @@ exports.postEditProduct = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -171,12 +172,10 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       "Product was deleted";
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleteing product failed" });
     });
 };
 
